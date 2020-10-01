2ca74ad387b51816a1219c4277299df48a8e08cc
Rodolphe DUPUY <rodolphe.dupuy@free.fr>
2020-10-01 21:45:44 +0200

    Initial Commit

---
+++ LoadImage/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  LoadImage
+//
+//  Created by Rodolphe DUPUY on 09/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var athens = "https://cdn.pixabay.com/photo/2016/12/08/13/42/athens-1891719_1280.jpg"
+    var xanthos = "http://bronze-age-towns.com/files/2020/09/entree-du-theatre-lycien-de-xanthos-300x166.jpg"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupActivity(true)
+    }
+    
+    func setupActivity(_ bool: Bool) {
+        activityIndicator.isHidden = !bool
+        bool ? activityIndicator.startAnimating() : activityIndicator.stopAnimating()
+    }
+
+    
+    @IBAction func loadImage(_ sender: UIButton) {
+        if sender.tag == 0 {
+            requestImageFrom(athens)
+        } else {
+            requestImageFrom(xanthos)
+        }
+    }
+    
+    func requestImageFrom(_ urlString: String) {
+        if !activityIndicator.isAnimating {
+            setupActivity(true)
+        }
+        if let url = URL(string: urlString) {
+            myImageView.image = nil
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                DispatchQueue.main.async {
+                    self.setupActivity(false)
+                    if let d = data {
+                        let image = UIImage(data: d)
+                        self.myImageView.image = image
+                    }
+                    if let r = response {
+                        print("Response: \(r)")
+                    }
+                    if let e = error {
+                        print("Erreur: \(e.localizedDescription)")
+                    }
+                }
+            }.resume()
+        }
+    }
+    
+}
